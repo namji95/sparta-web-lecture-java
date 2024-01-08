60e04df5204398a72223deada25fa859c58aa119
namji <skawlgns07@naver.com>
2024-01-08 20:22:04 +0900

    2024.01.08 자바 반복문 강의 정리
    while문 기본 문법 및 작성법 정리
    while문 사용법 정리

--- src/Second/WhileGrammar.java
+++ src/Second/WhileGrammar.java
@@ -1,2 +1,34 @@
-package Second;public class WhileGrammar {
+package Second;
+
+public class WhileGrammar {
+    public static void main(String[] args) {
+        // while 문 : ~하는 동안
+        int number1 = 0;
+        while(number1 < 3) {
+            number1++;
+            System.out.println(number1 + "출력");
+        }
+        /*
+        출력
+        1출력
+        2출력
+        3출력
+         */
+
+        // do-while 문
+        /*
+        do {
+            이 로직을 먼저 수행합니다.
+        } while (조건);
+         */
+        int number2 = 4;
+        do {
+            System.out.println(number2 + "출력");
+        } while(number2 < 3); // 연산을 한번 수행 후 조건문 체크
+        /*
+        출력
+        4출력
+         */
+
+    }
 }
