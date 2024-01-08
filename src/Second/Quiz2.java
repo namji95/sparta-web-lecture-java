5cd8d4b6a5b74804a5a46e3662446d5c92520096
namji <skawlgns07@naver.com>
2024-01-08 20:37:54 +0900

    2024.01.08 강의에서 배운 조건문, 반복문을 이용한 퀴
    if문을 이용한 가위바위보 게임 콘솔에 출력해보기 다중 if문 사용해서 구현
    for문을 이용한 구구단 게임 콘솔에 출력해보기 다중for문 사용하여
    구구단이 실행과 동시에 보여주는 것과 내가 타이핑한 구구단을 출력하는 두가지 방법으로 구현했습니다.

--- src/Second/Quiz2.java
+++ src/Second/Quiz2.java
@@ -1,2 +1,104 @@
-package Second;public class Quiz2 {
+package Second;
+
+import java.util.Scanner;
+
+public class Quiz2 {
+    // 구구단 만들어 보기
+    public static void main(String[] args) {
+        // 구구단 생성기
+        for (int i = 2; i <= 9; i++) { // 구구단 첫번째 지수 i
+            for (int j = 2; j <= 9; j++) { // 구구단 두번째 지수 j
+                System.out.println(i + " X " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+
+        // 선택적 구구단 생성기
+        // 내가 입력한 값에 따른 구구단 출력해보기
+        Scanner sc = new Scanner(System.in);
+        int passNum = sc.nextInt(); // 출력제외할 구구단수 값
+        for (int i = 2; i <= 9; i++) {
+            if (i == passNum) {
+                continue;
+            }
+            for (int j = 2; j <= 9; j++) {
+                System.out.println(i + " X " + j + " = " + (i * j));
+            }
+        }
+    }
 }
+
+/*
+첫 번째 구구단 출력 (두 번째 구구단도 값을 입력하면 그 값에 대한 출력이 밑 출력과 동일합니다.)
+2 X 2 = 4
+2 X 3 = 6
+2 X 4 = 8
+2 X 5 = 10
+2 X 6 = 12
+2 X 7 = 14
+2 X 8 = 16
+2 X 9 = 18
+
+3 X 2 = 6
+3 X 3 = 9
+3 X 4 = 12
+3 X 5 = 15
+3 X 6 = 18
+3 X 7 = 21
+3 X 8 = 24
+3 X 9 = 27
+
+4 X 2 = 8
+4 X 3 = 12
+4 X 4 = 16
+4 X 5 = 20
+4 X 6 = 24
+4 X 7 = 28
+4 X 8 = 32
+4 X 9 = 36
+
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+
+6 X 2 = 12
+6 X 3 = 18
+6 X 4 = 24
+6 X 5 = 30
+6 X 6 = 36
+6 X 7 = 42
+6 X 8 = 48
+6 X 9 = 54
+
+7 X 2 = 14
+7 X 3 = 21
+7 X 4 = 28
+7 X 5 = 35
+7 X 6 = 42
+7 X 7 = 49
+7 X 8 = 56
+7 X 9 = 63
+
+8 X 2 = 16
+8 X 3 = 24
+8 X 4 = 32
+8 X 5 = 40
+8 X 6 = 48
+8 X 7 = 56
+8 X 8 = 64
+8 X 9 = 72
+
+9 X 2 = 18
+9 X 3 = 27
+9 X 4 = 36
+9 X 5 = 45
+9 X 6 = 54
+9 X 7 = 63
+9 X 8 = 72
+9 X 9 = 81
+ */
