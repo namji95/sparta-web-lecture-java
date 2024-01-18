6123729a3c2a39a38b806d1ee7ccc6a684ad9d45
namji <skawlgns07@naver.com>
2024-01-18 20:15:20 +0900

    3주차 강의 실습
    Feat: 지역변수와 전역변수 차이
    지역변수 사용해보기

--- src/Third/sample/Main.java
+++ src/Third/sample/Main.java
@@ -1,2 +1,20 @@
-package Third.sample;public class Main {
+package Third.sample;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.getNumber());
+        System.out.println(main.getNumber());
+    }
+
+    // 메서드
+    public int getNumber() {
+        // 지역변수 <-> 전역변수 (Static)
+        // 해당 메서드가 실행될 때마다 독립적인 값을 저장하고 관리합니다.
+        // 이 지역변수는 메서드 내부에서 정의될 때 생성됩니다.
+        // 이 메서드가 종료될 때 소멸됩니다.
+        int number =1;
+        number += 1; // number = number + 1
+        return number;
+    }
 }
