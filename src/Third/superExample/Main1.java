f0f6333e7e38fdc2f7393eee60c0d9475315bd55
namji <skawlgns07@naver.com>
2024-01-18 20:12:16 +0900

    3주차 강의 실습
    Feat: 상속을 위한 super 정리
    super 실습해보기
    this와 super로 부모클래스의 필드와 자식클래스의 필드 구분해보기
    super() 실습해보기
    super()에 부모클래스의 필드 파라미터로 입력해보기

--- src/Third/superExample/Main1.java
+++ src/Third/superExample/Main1.java
@@ -1,2 +1,16 @@
-package Third.superExample;public class Main1 {
+package Third.superExample;
+
+public class Main1 {
+    public static void main(String[] args) {
+        // 자식 클래스 스포츠카 객체를 생성합니다.
+        SportsCar1 sportsCar = new SportsCar1("Lamborghini", "Red", 400000000, "V12");
+        sportsCar.brakePedal();
+        sportsCar.horn();
+
+        // 자식 클래스의 생성자를 통해 부모 클래스의 생성자가 호출되어 필드값이 초기화 되었는지 확인
+        System.out.println("sportsCar.getModel() = " + sportsCar.getModel()); // Lamborghini
+        System.out.println("sportsCar.getColor() = " + sportsCar.getColor()); // Red
+        System.out.println("sportsCar.getPrice() = " + sportsCar.getPrice()); // 4.0E8
+
+    }
 }
--- src/Third/superExample/SportsCar1.java
+++ src/Third/superExample/SportsCar1.java
@@ -1,2 +1,27 @@
-package Third.superExample;public class SportsCar1 {
+package Third.superExample;
+
+public class SportsCar1 extends Car1{
+    String engine;
+
+    public SportsCar1(String model, String color, double price, String engine) {
+        // this.engine = engine; // 오류 발생
+        super(model, color, price);
+        this.engine = engine;
+    }
+
+    public void booster() {
+        System.out.println("엔진 " + engine + " 부앙~\n");
+    }
+
+    @Override
+    public double brakePedal() {
+        speed = 100;
+        System.out.println("스포츠카에 브레이크란 없다");
+        return speed;
+    }
+
+    @Override
+    public void horn() {
+        booster();
+    }
 }
