41637f0638469176b5bd34debd7f643ec0ca9864
namji <skawlgns07@naver.com>
2024-01-19 14:22:10 +0900

    자바 3주차 강의 상속
    Feat: 자바 상속 실습을 위한 extendsExample 패키지와 구현할 클래스 생성
    상속은 부모 클래스의 필드와 메서드를 자식 클래스에 물려는 것이 이론적 의미입니다.
    클래스끼리의 상속은 다중 상속이 불가능하고 상속을 위해 자식 클래스명 옆에 extends를 작성하고 뒤에 부모 클래스명을 작성합니다.

--- src/Third/extendsExample/SportsCar.java
+++ src/Third/extendsExample/SportsCar.java
@@ -1,2 +1,9 @@
-package Third.extendsExample;public class SportsCar {
+package Third.extendsExample;
+
+// 자식 클래스
+public class SportsCar extends Car{
+    String engine;
+    public void booster() {
+        System.out.println("엔진 " + engine + " 부앙~\n");
+    }
 }
