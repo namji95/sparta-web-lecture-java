560fbb529f3fd3082319e81e0a867e6a1a6d6256
namji <skawlgns07@naver.com>
2024-01-19 14:17:38 +0900

    자바 3주차 강의
    Feat: 상속에 대해 알아보기 위한 hasExample 패키지와 class 생성
    각 클래스 별 상속 관계인지 포함 관계인지 알아보는 실습
    자동차를 예로 실습
    상속과 포함의 이론적 예시
    상속 관계: is ~ a (~은 ~이다.)
    포함 관계: has ~ a(~은 ~을 가지고 있다.)

--- src/Third/hasExample/Tire.java
+++ src/Third/hasExample/Tire.java
@@ -1,2 +1,11 @@
-package Third.hasExample;public class Tire {
+package Third.hasExample;
+
+public class Tire {
+    String company; // 타이어 회사
+    double price; // 타이어 가격
+
+    public Tire(String company, double price) {
+        this.company = company;
+        this.price = price;
+    }
 }
